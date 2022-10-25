3ddc3cdf0e837fd73b8e5a87aac6d124891b71ab
Kishor Malakar <malakarkishor24@gmail.com>
2022-10-25 22:46:54 +0530

    Revert "u"
    This reverts commit 6349455e9c6c45ebdb02964f6b89b477644400f4.

--- src/App.js
+++ src/App.js
@@ -1,60 +1,23 @@
-import React, {useState} from "react";
-import {Routes, Route} from "react-router-dom";
-import {
-  About,
-  Dark,
-  Footer,
-  Header,
-  Skills,
-  Work, 
-} from "./container";
-import { Navbar } from "./components";
-import "./App.scss";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [mode, setMode] = useState("light")
-  // document.body.style.backgroundColor = "#f7faff";
-  const toggleSwitch = () => {
-    if (mode === "light") {
-      setMode("dark");
-      // document.body.style.backgroundColor = "hsl(240deg 73% 25%)";
-      document.body.style.color = "var(--lightblue-color)";
-    } else {
-      setMode("light");
-      // document.body.style.backgroundColor = "white";
-      document.body.style.color = "black";
-    }
-  };
-  // let Component
-  // switch(window.location.pathname){
-  //   case "/":
-  //     Component = <App/>
-  //     break
-  //     case "/Home":
-  //       Component = <Header mode={mode}toggleSwitch={toggleSwitch}/>
-  //       break
-  //       case "/About":
-  //         Component = <About mode={mode}toggleSwitch={toggleSwitch}/>
-  //         break
-  // }
   return (
-    <div className="app">
-      <Navbar mode={mode} />
-      <Dark mode={mode} toggleSwitch={toggleSwitch} />
-      {/* {Component} */}
-      <Routes>
-        <Route path="/" element={<Header mode={mode}toggleSwitch={toggleSwitch}/>}/>
-        <Route path="/Home" element={<Header mode={mode}toggleSwitch={toggleSwitch}/>}/>
-        <Route path="/About" element={<About mode={mode}toggleSwitch={toggleSwitch}/>}/>
-        <Route path="/Work" element={<Work mode={mode}toggleSwitch={toggleSwitch}/>}/>
-        <Route path="/Skills" element={<Skills mode={mode}toggleSwitch={toggleSwitch}/>}/>
-        <Route path="/Contact" element={<Footer mode={mode}toggleSwitch={toggleSwitch}/>}/>
-      </Routes>
-       {/* <Header mode={mode}toggleSwitch={toggleSwitch}/>
-       <About mode={mode}toggleSwitch={toggleSwitch}/>
-       <Work />
-       <Skills />
-       <Footer /> */}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export {default as Navbar} from './Navbar/Navbar'
-export {default as SocialMedia} from './SocialMedia'
